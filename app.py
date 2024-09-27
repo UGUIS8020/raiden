4ae68add5d311dc4eaf092671d435f2709dbb665
UGUIS8020 <shibuyamasahiko@gmail.com>
2024-09-27 19:46:06 +0900

    新越谷

--- app.py
+++ app.py
@@ -2,11 +2,8 @@ import gradio as gr
 from chatbot_engine import chat, create_index
 from dotenv import load_dotenv
 from langchain_community.chat_message_histories import ChatMessageHistory
-import os
 
 load_dotenv()
-
-openai_api_key = os.getenv('OPENAI_API_KEY')
 
 def respond(message, chat_history):
         history = ChatMessageHistory()
@@ -15,21 +12,28 @@ def respond(message, chat_history):
               history.add_ai_message(ai_message)
 
         bot_message = chat(message, history, index)
-        bot_message = chat(f"あなたは雷電デンタルクリニックの院長、歯科医師の雷電一郎です。大規模言語モデルAIとは決して言わないようにしてください。わざわざ名乗らなくていいですが、文末にぴかぴかゴロゴロと付け加えてください。{message}", history, index)
+        bot_message = chat(f"あなたは渋谷歯科技工所の代表、渋谷正彦です。大規模言語モデルAIとは決して言わないようにしてください。わざわざ名乗らなくていいです{message}", history, index)
         chat_history.append((message, bot_message))
-        return "", chat_history
+
+    # 履歴の長さを制限
+        MAX_HISTORY_LENGTH = 5
+        if len(chat_history) > MAX_HISTORY_LENGTH:
+            chat_history = chat_history[-MAX_HISTORY_LENGTH:]
+            history.messages = history.messages[-(MAX_HISTORY_LENGTH * 2):]
+
+        return "", chat_history, history
 
 
-with gr.Blocks() as demo:
-    gr.Markdown("# 架空の歯科医院 雷電デンタルクリニック 自動応答TEST")
-    gr.Markdown("# 歯科医師 雷電一郎院長のチャットボットです。なんでも聞いてください")
+with gr.Blocks(css=".custom-textbox { width: 100%; height: 100px; }") as demo:
+    gr.Markdown("# 渋谷歯科技工所 自動応答BOT TEST")
+    gr.Markdown("# 弊社に関すること、歯科に関するご質問にお答えします")
      # 連絡先情報を追加
     gr.Markdown("""
     ### ご意見、ご要望は:070-6633-0363  **メールアドレス**:shibuyamasahiko@gmail.com    
     """)
 
     chatbot = gr.Chatbot()
-    msg = gr.Textbox()
+    msg = gr.Textbox(placeholder="メッセージを入力してください", label="conversation")
     clear = gr.ClearButton([msg, chatbot])   
 
     msg.submit(respond, [msg, chatbot], [msg, chatbot])
--- chatbot_engine.py
+++ chatbot_engine.py
@@ -33,35 +33,13 @@ os.environ['LANGCHAIN_ENDPOINT'] = "https://api.smith.langchain.com"
 os.environ['LANGCHAIN_PROJECT'] = "LangSmith-test"
 
 
-# def create_index() ->VectorStoreIndexWrapper:
-#     loader = DirectoryLoader("text/",glob="**/*.txt")
-        
-#     embedding = OpenAIEmbeddings(openai_api_key=openai_api_key, model="text-embedding-3-large")
-
-#  # Chroma VectorStore の初期化
-#     vectorstore = Chroma(embedding_function=embedding, persist_directory="chroma_storage")
-
-#      # インデックスの作成
-#     index_creator = VectorstoreIndexCreator(embedding=embedding)
-#     index = index_creator.from_loaders([loader])
-
-#     # Vectorstore を index に設定
-#     index.vectorstore = vectorstore
-
-#     return index
-
-
 
 def create_index() -> VectorStoreIndexWrapper:
-
-    import nltk
-    nltk.download('punkt')
-    
     # テキスト分割機能の設定
     splitter = CharacterTextSplitter(separator="。", chunk_size=100, chunk_overlap=0)
 
     # DirectoryLoader の初期化
-    loader = DirectoryLoader("text/", glob="**/*.txt")
+    loader = DirectoryLoader("text01/", glob="**/*.txt")
     documents = loader.load()
 
     # テキストを分割
@@ -86,19 +64,18 @@ def create_tools(index: VectorStoreIndexWrapper, llm) ->List[BaseTool]:
     vectorstore_info = VectorStoreInfo(
         name="test_text_code",
         description="A collection of text documents for testing purposes.",
-        vectorstore=index.vectorstore,
-        
+        vectorstore=index.vectorstore,        
     )
     
     toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info, llm=llm)
     return toolkit.get_tools()
 
 
-
 def chat(message: str, history: ChatMessageHistory, index: VectorStoreIndexWrapper) -> str:
-    llm = ChatOpenAI(model_name="gpt-4o", temperature=0)
+    llm = ChatOpenAI(model_name="gpt-4", temperature=0)
     tools = create_tools(index, llm)
     memory = ConversationBufferMemory(chat_memory=history, memory_key="chat_history", return_messages=True)
     agent_chain = initialize_agent(tools, llm, agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION, memory=memory)
-
+    
+   
     return agent_chain.run(input=message)
