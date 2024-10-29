cb2fee9233e99e415892993e49413a7fb15ef1a9
UGUIS8020 <shibuyamasahiko@gmail.com>
2024-10-30 01:01:35 +0900

    新越谷

--- app.py
+++ app.py
@@ -12,7 +12,10 @@ def respond(message, chat_history):
               history.add_ai_message(ai_message)
 
         # bot_message = chat(message, history, index)
-        bot_message = chat(f"あなたは渋谷歯科技工所の代表です。日本語で答えてください{message}", history, index)
+        bot_message = chat(f"まずはPineconeに保存されているデータから検索して答えを探して、"
+                      f"見つからなければopenaiで回答してください。日本語で答えてください。{message}", 
+                      history, 
+                      index)
         chat_history.append((message, bot_message))
 
     # 履歴の長さを制限
