f174ccd902ee0bca6bdcb3b7eebba9f82ff967ae
UGUIS8020 <shibuyamasahiko@gmail.com>
2025-02-04 17:42:54 +0900

    新越谷

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def respond(message, chat_history):
 
 
 # with gr.Blocks(css=".custom-textbox { width: 100%; height: 100px; border: 2px solid #2c3e50; }") as demo:
-with gr.Blocks(css=".gradio-container {background-color: #FFE4B5}") as demo:
+with gr.Blocks(css=".gradio-container {background-color:rgb(248, 230, 199)}") as demo:
     gr.Markdown("## 渋谷歯科技工所 自動応答BOT")
     gr.Markdown("## 自家歯牙移植、歯牙再植について専門的に応対するチャットボット")
     # 連絡先情報を追加
