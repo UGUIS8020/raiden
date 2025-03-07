39b31586ab275abefcabf0dfa7a034906a69229f
UGUIS8020 <shibuyamasahiko@gmail.com>
2025-03-07 09:14:10 +0900

    プログラムコードの更新

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ with gr.Blocks(css=".gradio-container {background-color:rgb(248, 230, 199)}") as
     msg.submit(respond, [msg, chatbot], [msg, chatbot])
 
 if __name__ == "__main__":
-    demo.launch(server_name="127.0.0.1", server_port=7860)
+    demo.launch(server_name="0.0.0.0", server_port=7860)
     # demo.launch()
