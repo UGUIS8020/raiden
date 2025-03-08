025ef51017967c50a605f561a9ccf0888e1cadb4
UGUIS8020 <shibuyamasahiko@gmail.com>
2025-03-08 11:53:03 +0900

    新越谷

--- app.py
+++ app.py
@@ -52,9 +52,9 @@ with gr.Blocks(css=".gradio-container {background-color:rgb(248, 230, 199)}") as
     clear = gr.ClearButton([msg, chatbot])
     msg.submit(respond, [msg, chatbot], [msg, chatbot])
 
-# if __name__ == "__main__":
-#     demo.launch(server_name="0.0.0.0", server_port=7860)
-
 if __name__ == "__main__":
-    demo.launch(server_name="127.0.0.1", server_port=7860)
+    demo.launch(server_name="0.0.0.0", server_port=7860)
+
+# if __name__ == "__main__":
+#     demo.launch(server_name="127.0.0.1", server_port=7860)
     
