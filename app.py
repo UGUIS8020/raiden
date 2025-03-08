2542122cca50dd0143160c4865346498fbff4f33
UGUIS8020 <shibuyamasahiko@gmail.com>
2025-03-08 11:51:35 +0900

    新越谷

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def respond(message, chat_history):
 with gr.Blocks(css=".gradio-container {background-color:rgb(248, 230, 199)}") as demo:    
     gr.Markdown("## 自家歯牙移植、歯牙再植について専門的に応対します")
     # 連絡先情報を追加
-    gr.Markdown("## ただいまテスト中。機能を追加 RAIDEN.v2:近日公開予定")
+    gr.Markdown("## RAIDEN.v1.52 :RAIDEN.v2.0:近日公開予定")
     gr.Markdown("""
     ### チャットボットに関するご意見、ご要望は:070-6633-0363  **email**:shibuya8020@gmail.com    
     """)    
